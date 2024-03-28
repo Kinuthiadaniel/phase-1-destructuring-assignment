765907a598c16e73ff11834d226386e7ec70906c
Kinuthiadaniel <dankinuthia17@gmail.com>
2024-03-28 12:15:22 +0300

    intial commit

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const farmAnimals = 'cow horse sheep pig chicken';
-
+const [moo, neigh, baa, oink,cluck] = farmAnimals.split(' ')
+console.log(moo);
+const [bessie, ,dolly,babe,little] = farmAnimals.split(' ')
+const [blackAndWhite, ,black,pink,]= farmAnimals.split(' ')
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
-
+const [red, orange, yellow, green, blue, indigo, violet] = colors
+const [r,o,y,g,b, ,v] = colors
+const [, , , , ,indg, ,]= colors
+console.log(indg)
 const muppet = {
   muppetName: 'Miss Piggy',
   color: 'pink',
@@ -9,7 +15,7 @@ const muppet = {
   job: 'Cast member of The Muppet Show',
   partner: 'Kermit'
 };
-
+const {muppetName,color,song,job,partner} = muppet
 const nestedMuppet = {
   nestedName: 'Kermit',
   nestedColor: 'green',
@@ -24,10 +30,12 @@ const nestedMuppet = {
   nestedJob: 'Host of The Muppet Show',
   nestedPartner: 'Miss Piggy'
 };
-
+const {song2,song4} = nestedMuppet.album.theMuppetMovie
+const {nestedJob, nestedPartner}= nestedMuppet
 // Strings
 
 // 1. Use destructuring to assign appropriate variables based on the sounds animals make.
+
 
 // 2. Bolt the horse wandered off, so just give us four animals, and let's name them bessie, dolly, babe, and little.
 
